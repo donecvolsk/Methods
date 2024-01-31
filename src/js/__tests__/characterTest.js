b0454a5da68fae6071da8212f1586ea148069b90
donecvolsk <donecvolsk@yandex.ru>
2024-01-31 15:15:27 +0400

    ДЗ Методы выполнено 31012024

--- src/js/__tests__/characterTest.js
+++ src/js/__tests__/characterTest.js
@@ -20,9 +20,64 @@ test('testing  class Character features', () => {
     expect(errorName).toThrow(Error);
     })
 
-    test('testing  class Character error from type ', () => {
-        function errorName() {
-          new Character('Bobik', 'Bowmann');
-        }
-        expect(errorName).toThrow(Error);
-        })
+  test('testing  class Character error from type ', () => {
+    function errorName() {
+      new Character('Bobik', 'Bowmann');
+    }
+    expect(errorName).toThrow(Error);
+    })
+
+  test('testing  class Character metod levelUP', () => {
+    const resultingObject = {
+      attack: 30,
+      defence: 30,
+      health: 100,
+      level: 2,
+      name: "Bobik",
+      type: "Bowman",
+          }
+          
+    const result = new Character('Bobik', 'Bowman');
+    result.health = 100;
+    result.attack = 25;
+    result.defence = 25;
+    result.level = 1;
+    result.levelUp();
+           
+    expect(result).toEqual(resultingObject);      
+          
+    })
+
+  test('testing  class Character error from levelUP ', () => {
+    function errorName() {
+      const result = new Character('Bobik', 'Bowman');
+      result.health = 0;
+      result.attack = 25;
+      result.defence = 25;
+      result.level = 1;
+      result.levelUp();
+      }
+    expect(errorName).toThrow(Error);
+  })
+
+  test('testing  class Character metod damage', () => {
+    const resultingObject = {
+      attack: 30,
+      defence: 30,
+      health: 86,
+      level: 2,
+      name: "Bobik",
+      points: 20,
+      type: "Bowman",
+      }
+              
+  const result = new Character('Bobik', 'Bowman');
+  result.health = 100;
+  result.attack = 25;
+  result.defence = 25;
+  result.level = 1;
+  result.levelUp();
+  result.damage(20);          
+  expect(result).toEqual(resultingObject);      
+              
+  })
